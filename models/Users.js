af6aa3aef02b6b152950aa83096c0a6ee44ce528
Charles Hargrave <charles.hargrave.jr@gmail.com>
2022-10-10 18:59:53 -0600

    added user schema

---
+++ models/Users.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: (email) => {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+        },
+        message: (email) =>
+          `${email.value} not a valid email. Please try again.`,
+      },
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const Users = model("user", userSchema);
+
+module.exports = Users;
