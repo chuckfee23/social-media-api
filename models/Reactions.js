3d9a2ae8d4f3bf30f4307a8e576f4dee546e771d
Charles Hargrave <charles.hargrave.jr@gmail.com>
2022-10-11 22:17:33 -0600

    edited timestamping to be more user-friendly

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,11 +1,12 @@
 const { Schema } = require("mongoose");
+const Mongoose = require("mongoose");
 
 const reactionSchema = new Schema(
   {
     reactionId: {
       type: Schema.Types.ObjectId,
       default: function () {
-        return new ObjectId();
+        return new Mongoose.Types.ObjectId();
       },
     },
     reactionBody: { type: String, required: true, maxLength: 280 },
